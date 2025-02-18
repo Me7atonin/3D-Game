cf4f16a7e2e700b962bac572d1d66b2ac519cc66
Devin <mcmanusdevin2008@gmail.com>
2025-02-18 12:04:23 -0800

    added interact tag and test script

---
+++ Escape From The Office/Assets/Devin/RaycastTest.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+public class Interaction : MonoBehaviour
+{
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2))
+        {
+            if (hit.collider.tag.Equals("Interact"))
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    
+                }
+            }
+        }
+    }
+}
