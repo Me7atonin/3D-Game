2ef10c98b67d5b3912b681141efdf4efbe33917b
Devin <mcmanusdevin2008@gmail.com>
2025-02-18 12:17:42 -0800

    making door animation

--- Escape From The Office/Assets/Devin/RaycastTest.cs
+++ Escape From The Office/Assets/Devin/RaycastTest.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 using System.Collections;
 public class Interaction : MonoBehaviour
 {
-    void Update()
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+        void Update()
     {
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2))
